cac50b04894565a0cf4900758d12ca56136aa280
Laur8825 <laur8825@gmail.com>
2024-01-11 21:10:02 +0200

    first commit

---
+++ src/test/java/com/realestate/propertylisting/PropertylistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realestate.propertylisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertylistingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
